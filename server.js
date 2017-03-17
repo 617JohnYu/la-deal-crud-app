2670362de92caf648d9733fb498981085925af26
John Yu <you@example.com>
2017-03-17 14:41:45 -0700

    commiting admin changes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require(
+    'express'
+)
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 8080
+
+app.use((req, res) => {
+    res.send('GGGGGGGGGGGGGGG')
+})
+
+app.listen(port, () => {
+    console.log('RAID BOSS IS RUNNING ON port: ' + port)
+})
